d41c524c9cb56c86445e53eff5ecbc4faff5b9d4
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-02 22:43:51 +0900

    [Test] コンソール出力

--- Test.cs
+++ Test.cs
@@ -7,7 +7,12 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        var test = "TEST!!";
+        Debug.Log(test); //test変数をログに出力
+        Debug.Log(test.Length); //test変数の長さを出力
+
+        test = null;
+        Debug.Log(test.Length); //空っぽなのでエラーが発生(NullReferenceException)
     }
 
     // Update is called once per frame
