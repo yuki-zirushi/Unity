c501b099f2ec0323762d232922f0e1ae42df3ebb
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-06 14:48:12 +0900

    [Update]

--- 敵キャラ自動追従.cs
+++ 敵キャラ自動追従.cs
@@ -4,7 +4,7 @@ using UnityEngine.AI;
 [RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMove : MonoBehaviour
 {
-  [SerializeField] private PlayerController playerController;
+  //[SerializeField] private PlayerController playerController;
   private NavMeshAgent _agent;
 
   private void Start()
@@ -12,8 +12,18 @@ public class EnemyMove : MonoBehaviour
     _agent = GetComponent<NavMeshAgent>(); //NavMeshAgentを保持しておく
   }
 
-  private void Update()
+  //private void Update()
+  //{
+  //  _agent.destination = playerController.transform.position;
+  //}
+
+  //CollisionDetectorのonTriggerStayにセットし、衝突判定を受け取るメソッド
+  public void OnDetectorObject(Collider collider)
   {
-    _agent.destination = playerController.transform.position;
+    //検知したオブジェクトに「Player」のタグがついていれば、そのオブジェクトを追いかける
+    if (collider.CompareTag("Player"))
+    {
+      _agent.destination = collider.transform.position;
+    }
   }
 }
