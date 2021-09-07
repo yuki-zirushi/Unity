2dff1c406fafee2d7482f34c3010cac8605ef120
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-07 12:08:56 +0900

    [Update]

--- 衝突検知.cs
+++ 衝突検知.cs
@@ -5,7 +5,13 @@ using UnityEngine.Events;
 [RequireComponent(typeof(Collider))]
 public class CollisionDetector : MonoBehaviour
 {
+  [SerializeField] private TriggerEvent onTriggerEnter = new TriggerEvent();
   [SerializeField] private TriggerEvent onTriggerStay = new TriggerEvent();
+
+  private void OnTriggerEnter(Collider other)
+  {
+    onTriggerEnter.Invoke(other);
+  }
 
   // Is TriggerがONで他のColliderと重なっているときは、このメソッドが常にコールされる
   private void OnTriggerStay(Collider other)
