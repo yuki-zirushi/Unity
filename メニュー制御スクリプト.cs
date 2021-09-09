025000374e2b42be9cb0f6aa39fc82ca4e5bfa3d
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-09 10:30:41 +0900

    [Create]

---
+++ メニュー制御スクリプト.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+  [SerializeField] private Button pauseButton;
+  [SerializeField] private GameObject pausePanel;
+  [SerializeField] private Button resumeButton;
+
+  [SerializeField] private Button itemsButton;
+  [SerializeField] private Button recipeButton;
+
+  private void Start()
+  {
+    pausePanel.SetActive(false); //ポーズのパネルは初期状態では非表示にしておく
+
+    pauseButton.onClick.AddListener(Pause);
+    resumeButton.onClick.AddListener(Resume);
+    itemsButton.onClick.AddListener(ToggleItemsDialog);
+    recipeButton.onClick.AddListener(ToggleRecipeDialog);
+  }
+
+  //ゲームを一時停止する
+  private void Resume()
+  {
+    Time.timeScale = 1; //また時間が流れるようにする
+    pausePanel.SetActive(false);
+  }
+
+  //アイテムウィンドウを開閉
+  private void ToggleItemsDialog()
+  {
+    //TODO 後で実装
+  }
+
+  //レシピウィンドウを開閉
+  private void ToggleRecipeDialog()
+  {
+    //TODO 後で実装
+  }
+}
