3ad1f3ed029a676f78412d2b29ca50748aa8cd1d
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-09 16:57:49 +0900

    [Fix]

--- 複数のライフゲージを管理するクラス.cs
+++ 複数のライフゲージを管理するクラス.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequierComponent(typeof(RectTransform))]
+[RequireComponent(typeof(RectTransform))]
 public class LifeGaugeContainer : MonoBehaviour
 {
   public static LifeGaugeContainer Instance
