5ec5e7e820c38a38a47525a59c33497c5ec7fb4d
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-13 00:05:10 +0900

    [Create]

---
+++ IkinokoBattle/Standard Assets fix(2).cs
@@ -0,0 +1,14 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI; //この行を追加
+
+namespace UnityStandardAssets.Utility
+{
+  public class SimpleActivatorMenu : MonoBehaviour
+  {
+    // An incredibly simple menu which, when given references
+    // to gameobjects in the scene
+    public Text camSwitchButton; 「GUIText」を「Text」に変更
+    public GameObject[] objects;
+  }
+}
