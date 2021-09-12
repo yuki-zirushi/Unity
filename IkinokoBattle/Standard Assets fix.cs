98c200bbafb6d552b5ee6b227b725976ff85c8b5
yuki-zirushi <yuko.12.151@gmail.com>
2021-09-12 23:51:10 +0900

    [Create]

---
+++ IkinokoBattle/Standard Assets fix.cs
@@ -0,0 +1,11 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+using UnityStandardAssets.CrosPlatformInput;
+using UnityEngine.UI; //この行を追加
+
+[RequireComponent(typeof (Image))] //「GUITexture」を「Image」に変更
+public class ForcedReset : MonoBehaviour
+{
+  private void Update()
+}
